ea28d906c9159c6ff6f5e39c9fa5f65c9ed81663
Adel Chouanine <a.chouanine@hotmail.fr>
2015-08-24 13:46:44 +0200

    modification controller home et correction route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,26 @@
 class UsersController < ApplicationController
   def home
+  	if session[:user_id]
+    @user = User.find(session[:user_id])
+    end
+
   end
 
   def login
+  	session[:test] = "Valeur de test"
   end
 
   def check
     @user = User.where(name: params[:name], password: params[:password]).first
     if @user
+      session[:user_id] = @user.id
       flash[:info] = "Bienvenue #{@user.name} !"
       redirect_to "/users/home"
     else
+      session[:user_id] = nil
       flash[:info] = "Échec de la connexion"
       redirect_to "/users/login"
     end
   end
+
 end
