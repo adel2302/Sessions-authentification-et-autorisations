2c1a338a20dcfdfb25229f8024964d1fac3823b3
Adel Chouanine <a.chouanine@hotmail.fr>
2015-08-24 12:25:22 +0200

    ajout post user/login dans route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'users/home'
-
   get 'users/login'
+  post 'users/login => 'users#check'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
