46a7a22773c70357ffa812116098066441a5dab2
Adel Chouanine <a.chouanine@hotmail.fr>
2015-08-24 12:20:53 +0200

    creation def check dans controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,15 @@ class UsersController < ApplicationController
 
   def login
   end
+
+  def check
+    @user = User.where(name: params[:name], password: params[:password]).first
+    if @user
+      flash[:info] = "Bienvenue #{@user.name} !"
+      redirect_to "/users/home"
+    else
+      flash[:info] = "Échec de la connexion"
+      redirect_to "/users/login"
+    end
+  end
 end
